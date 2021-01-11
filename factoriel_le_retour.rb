757b6c97bb6f4e4ed72e21ab1c880ff3f40c6968
Developpeur Account <myadressemailisthat@gmail.com>
2021-01-11 10:40:32 +0100

    add file

---
+++ factoriel_le_retour.rb
@@ -0,0 +1,27 @@
+argument = ARGV[0].to_i
+
+# argument = 6
+# 6 * 5 * 4 * 3 * 2 * 1
+
+if (ARGV.length != 1)
+    puts "#{__FILE__} requires one argument: #{__FILE__} number_of_steps"
+    exit
+end
+
+if argument <= 0
+    puts "#{__FILE__} requieres a number superior to 0"
+    exit
+end
+
+def factoriel(argument)
+    if argument == 0
+        return 0
+    elsif argument == 1
+        return argument
+    else
+        return argument * factoriel(argument-1)
+    end
+end
+
+puts factoriel argument
+
